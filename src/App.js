c6f1ae98058e6051c3dc9d6c1d8b4c145aef835b
barin <barin.ozmen@xerini.co.uk>
2023-10-04 12:29:21 +0300

    added linkedin and github page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import me from './public/me.png';
 import resume1 from './public/resume1-1.png'
 import resume2 from './public/resume2-1.png'
+import linkedin from './public/linkedin.png'
+import github from './public/github.png'
 import './App.css';
 import { useEffect, useState } from 'react';
 import 'bootstrap';
@@ -13,11 +15,11 @@ const PageOptions = {
   
 function App() {
 
-  const [page, setPage] = useState(PageOptions.Home)
+  const [page, setPage] = useState(PageOptions.Resume)
 
   const navigationBar = () => {
     return <>
-      <header className="App-header" onClick={() => { setPage(PageOptions.Home) }} style={{cursor: "pointer"}}>
+      <header className="App-header" onClick={() => { setPage(PageOptions.Resume) }} style={{cursor: "pointer"}}>
         <img src={me} className="MePic" alt="me-profile-picture"/>        
       </header>
       <span style={{ background: "white", fontSize: 10, borderRadius: 8, padding: 4, marginTop: 16, maxWidth: 340 }}>
@@ -31,9 +33,19 @@ function App() {
         <br/>
         I have been enjoying working as a team player throughout my working life. I am ready to do my best to improve each other as a team.
       </span>
-      <button className="HomeButton" style={{opacity: page === PageOptions.Resume ? 0.4: 1}} onClick={() => {setPage(PageOptions.Resume)}}>
-        Download Resumes
-      </button>
+      {/*<button className="HomeButton" style={{opacity: page === PageOptions.Resume ? 0.4: 1}} onClick={() => {setPage(PageOptions.Resume)}}>
+        Resumes
+      </button>*/}
+      <a href='https://linkedin.com/in/bar%C4%B1n-%C3%B6zmen-2b4233135' target='_blank'>
+        <button className="HomeButton">
+          Linkedin <img src={linkedin} style={{width: 24, marginTop: -5}} alt="linkedin-icon"/>
+        </button>
+      </a>
+      <a href='https://github.com/ozmenbrn' target='_blank'>
+        <button className="HomeButton">
+          Github <img src={github} style={{width: 18, marginTop: -5, marginLeft: 4}} alt="github-icon"/>
+        </button>
+      </a>
     </>
   }
 
